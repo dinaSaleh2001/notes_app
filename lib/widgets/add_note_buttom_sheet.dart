821f0eb39d11873fb134b1d8f3f823c6cd3fe48a
Your Name <you@example.com>
2024-02-07 01:26:36 +0200

    finish notes button sheet

--- lib/widgets/add_note_buttom_sheet.dart
+++ lib/widgets/add_note_buttom_sheet.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:store_app/constant.dart';
+import 'package:store_app/widgets/custom_button.dart';
 import 'package:store_app/widgets/custom_text_fild.dart';
 
 class AddNoteButtonSheet extends StatelessWidget {
@@ -10,23 +12,32 @@ class AddNoteButtonSheet extends StatelessWidget {
       padding: const EdgeInsets.symmetric(
         horizontal: 16,
       ),
-      child: Column(
-        children: [
-          SizedBox(
-            height: 32,
-          ),
-          CustomTextFild(
-            hintText: 'Title',
-            maxLine: 1,
-          ),
-          SizedBox(
-            height: 16,
-          ),
-          CustomTextFild(
-            hintText: 'Context',
-            maxLine: 5,
-          ),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 32,
+            ),
+            CustomTextFild(
+              hintText: 'Title',
+              maxLine: 1,
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            CustomTextFild(
+              hintText: 'Context',
+              maxLine: 5,
+            ),
+            SizedBox(
+              height: 32,
+            ),
+            CustomButton(),
+            SizedBox(
+              height: 16,
+            ),
+          ],
+        ),
       ),
     );
   }
