f6ce7f2a0d92ae7718779a980d6b41946cfd3f58
Your Name <you@example.com>
2024-02-07 02:06:36 +0200

    create edit notes view

--- lib/widgets/custom_notes_item.dart
+++ lib/widgets/custom_notes_item.dart
@@ -1,64 +1,76 @@
 import 'package:flutter/material.dart';
+import 'package:store_app/Screens/edite_notes_view.dart';
+import 'package:store_app/widgets/edit_view_body.dart';
 
 class NotesItem extends StatelessWidget {
   const NotesItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.only(
-        top: 24,
-        bottom: 24,
-        left: 16,
-      ),
-      decoration: BoxDecoration(
-        color: Color(0xffFFCC80),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: Text(
-              'Flutter tips',
-              style: TextStyle(
-                fontSize: 26,
-                color: Colors.black,
-              ),
-            ),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(
-                top: 16,
-                bottom: 16,
-              ),
-              child: Text(
-                'Build Your Career with Dina Saleh ',
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) {
+            return EditNoteView();
+          }),
+        );
+      },
+      child: Container(
+        padding: EdgeInsets.only(
+          top: 24,
+          bottom: 24,
+          left: 16,
+        ),
+        decoration: BoxDecoration(
+          color: Color(0xffFFCC80),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: Text(
+                'Flutter tips',
                 style: TextStyle(
-                  fontSize: 18,
-                  color: Colors.black.withOpacity(0.5),
+                  fontSize: 26,
+                  color: Colors.black,
+                ),
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(
+                  top: 16,
+                  bottom: 16,
+                ),
+                child: Text(
+                  'Build Your Career with Dina Saleh ',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.black.withOpacity(0.5),
+                  ),
+                ),
+              ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.delete,
+                  size: 30,
+                  color: Colors.black,
                 ),
               ),
             ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: Icon(
-                Icons.delete,
-                size: 30,
-                color: Colors.black,
+            Padding(
+              padding: const EdgeInsets.only(right: 24),
+              child: Text(
+                'May 21, 2022',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.black.withOpacity(0.4),
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 24),
-            child: Text(
-              'May 21, 2022',
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.black.withOpacity(0.4),
-              ),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
