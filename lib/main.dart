4c9933c713064d834d0222e835d48940cc8bc991
Your Name <you@example.com>
2024-02-07 23:35:42 +0200

    init hive and open notes box

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:store_app/Screens/notes_page.dart';
+import 'package:store_app/constant.dart';
 
-void main() {
-  runApp(const NotesApp());
+void main() async {
+  await Hive.initFlutter();
+
+  await Hive.openBox(kNotesBox);
+  runApp(
+    const NotesApp(),
+  );
 }
 
 class NotesApp extends StatelessWidget {
